ec0644dda8f4764c633c2662d630e996bcf5b4d5
ShimmyMySherbet <ShimmyMySherbet@gmail.com>
2019-08-04 23:01:32 +1000

    Improved Spotify Track Detection

--- YoutubeDownloader/SpotifyApiBridge.vb
+++ YoutubeDownloader/SpotifyApiBridge.vb
@@ -47,7 +47,6 @@ Public Class SpotifyApiBridge
                     Dim TrackDuration As Double = item.DurationMs
                     Dim DurationDifferance As Double = GetIntgralDifferance(VideoDuration, TrackDuration)
                     If DurationDifferance < TrackLogic.MaxDurationDifferance Then
-                        '/*todo: Create further verification logic/*'
                         SelectedResult = item
                     End If
                 End If
@@ -70,11 +69,92 @@ Public Class SpotifyApiBridge
                 Dim TrackDuration As Double = item.DurationMs
                 Dim DurationDifferance As Double = GetIntgralDifferance(VideoDuration, TrackDuration)
                 If DurationDifferance < TrackLogic.MaxDurationDifferance Then
-                    '/*todo: Create further verification logic/*'
                     SelectedResult = item
                 End If
             End If
         Next
+
+
+        If IsNothing(SelectedResult) Then
+
+            If MexMedia.Artist <> "" Then
+                Dim Redmedianame As String = MexMedia.Name
+                If MexMedia.Name.ToLower.Contains("ft") Or MexMedia.Name.ToLower.Contains("feat") Then
+                    Dim resp As String = MexMedia.Name.ToLower.Replace("ft", vbNewLine).Replace("feat", vbNewLine)
+                    Redmedianame = resp.Split(vbNewLine)(0)
+                End If
+
+                Dim RedmediaArtist As String = MexMedia.Artist
+                If MexMedia.Artist.ToLower.Contains("ft") Or MexMedia.Artist.ToLower.Contains("feat") Then
+                    Dim resp As String = MexMedia.Artist.ToLower.Replace("ft", vbNewLine).Replace("feat", vbNewLine)
+                    RedmediaArtist = resp.Split(vbNewLine)(0)
+                End If
+
+                Dim FtSearchname As String = RedmediaArtist & " " & Redmedianame
+
+
+                Dim FtSearchResults As SearchItem = Spotify.SearchItems(FtSearchname, Enums.SearchType.Track)
+                For Each item In FtSearchResults.Tracks.Items
+                    If IsNothing(SelectedResult) Then
+                        Dim TrackDuration As Double = item.DurationMs
+                        Dim DurationDifferance As Double = GetIntgralDifferance(VideoDuration, TrackDuration)
+                        If DurationDifferance < TrackLogic.MaxDurationDifferance Then
+                            SelectedResult = item
+                        End If
+                    End If
+                Next
+            Else
+
+                Dim Redmedianame As String = MexMedia.Name
+                If MexMedia.Name.ToLower.Contains("ft") Or MexMedia.Name.ToLower.Contains("feat") Then
+                    Dim resp As String = MexMedia.Name.ToLower.Replace("ft", vbNewLine).Replace("feat", vbNewLine)
+                    Redmedianame = resp.Split(vbNewLine)(0)
+                End If
+
+                Dim searchpatten1 As String = Video.Author.Split("-")(0) & " " & Redmedianame
+                Dim searchpatten2 As String = Redmedianame
+
+                Dim p1SearchResults As SearchItem = Spotify.SearchItems(searchpatten1, Enums.SearchType.Track)
+                For Each item In p1SearchResults.Tracks.Items
+                    If IsNothing(SelectedResult) Then
+                        Dim TrackDuration As Double = item.DurationMs
+                        Dim DurationDifferance As Double = GetIntgralDifferance(VideoDuration, TrackDuration)
+                        If DurationDifferance < TrackLogic.MaxDurationDifferance Then
+                            SelectedResult = item
+                        End If
+                    End If
+                Next
+
+
+                If IsNothing(SelectedResult) Then
+                    Dim p2SearchResults As SearchItem = Spotify.SearchItems(searchpatten2, Enums.SearchType.Track)
+                    For Each item In p2SearchResults.Tracks.Items
+                        If IsNothing(SelectedResult) Then
+                            Dim TrackDuration As Double = item.DurationMs
+                            Dim DurationDifferance As Double = GetIntgralDifferance(VideoDuration, TrackDuration)
+                            If DurationDifferance < TrackLogic.MaxDurationDifferance Then
+                                SelectedResult = item
+                            End If
+                        End If
+                    Next
+                End If
+
+
+
+            End If
+
+
+
+
+
+
+
+
+
+        End If
+
+
+
         Return SelectedResult
     End Function
     Public Function GetIntgralDifferance(left As Double, right As Double) As Double
